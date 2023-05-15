9eff46ca3c69be9a2837bc9be1d0947a35bd8bc9
Hessamoddin A Shokravi <ahs502@gmail.com>
2023-05-15 18:40:43 +0330

    Remove Google Cloud credential secrets into the dotenv file

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,8 +1,8 @@
 import { SERVER_BASE_URL } from '../../shared/constants'
 
 export const GOOGLE_CLOUD_CREDENTIAL_WEB_CLIENT = {
-  client_id: '783889808992-v3tj0kuhrm1dum1veouc22tppjqsqthm.apps.googleusercontent.com',
-  project_id: 'zettel-export-to-docs',
-  client_secret: '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',
+  client_id: process.env.GOOGLE_CLOUD_CREDENTIAL_WEB_CLIENT_CLIENT_ID ?? '',
+  project_id: process.env.GOOGLE_CLOUD_CREDENTIAL_WEB_CLIENT_PROJECT_ID ?? '',
+  client_secret: process.env.GOOGLE_CLOUD_CREDENTIAL_WEB_CLIENT_CLIENT_SECRET ?? '',
   redirect_uri: `${SERVER_BASE_URL}/static/authenticated.html`,
 } as const
